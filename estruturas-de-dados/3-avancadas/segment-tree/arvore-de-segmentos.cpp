0bc9e32c175692fde0f8fdf963d0318198ba647c
Gabriel Bergoc <gabriel.bergoc@outlook.com>
2022-08-24 21:17:29 -0300

    Generic segment tree

--- estruturas-de-dados/3-avancadas/segment-tree/arvore-de-segmentos.cpp
+++ estruturas-de-dados/3-avancadas/segment-tree/arvore-de-segmentos.cpp
@@ -6,7 +6,7 @@
  * @date 2022-08-20
  *
  * @copyright Copyright (c) 2022
- * 
+ *
  * h = floor(log2(f)) (f = n. folhas, h = altura)
  * N = 2^(h + 1) - 1 (N = n. elementos)
  */
@@ -18,16 +18,16 @@ const int MAX_N = 1E5;              // max n. of leaves
 const int H = ceil(log2(MAX_N));    // max height
 const int TOTAL = pow(2, H + 1);    // max n. of nodes in tree
 
-int stocksValues[MAX_N]{0};
-int segTree[TOTAL]{0};
+int values[MAX_N]{ 0 };
+int segTree[TOTAL]{ 0 };
 
-// update value of stock at index <POS> with <NEW_VALUE> and paths of segment tree
+// update value at index <POS> with <NEW_VALUE> and path of segment tree
 void update(int iNode, int i, int j, int POS, int NEW_VALUE)
 {
     if (i == j) // we found the leaf in desired position
     {
-        segTree[iNode] = i;
-        stocksValues[POS] = NEW_VALUE;
+        // update values
+        // update segTree node
     }
     else    // keep searching
     {
@@ -44,24 +44,13 @@ void update(int iNode, int i, int j, int POS, int NEW_VALUE)
             update(r, m + 1, j, POS, NEW_VALUE);
         }
 
-        int iMinLeft = segTree[l];  // index of the minimum element of left subtree
-        int iMinRight = segTree[r]; // index of the minimum element of right subtree
-
-        // update current node to hold the index of it's minimum leaf
-        if (stocksValues[iMinLeft] < stocksValues[iMinRight])
-        {
-            segTree[iNode] = iMinLeft;
-        }
-        else
-        {
-            segTree[iNode] = iMinRight;
-        }
+        // update current segTree node
 
     }
 }
 
-// retrieve the index of the minimum stock value in interval [A, B]
-int minLeaf(int iNode, int i, int j, int A, int B)
+// retrieve wanted info in interval [A, B]
+int retrieve(int iNode, int i, int j, int A, int B)
 {
     // Case 1: [i, j]⊆[A, B] -> return the value of this node
     if (A <= i && j <= B)
@@ -75,26 +64,14 @@ int minLeaf(int iNode, int i, int j, int A, int B)
         return -1;
     }
 
-    // Case 3: [i, j]∩[A, B] ≠ ∅ but [i, j]⊄[A, B] -> return the best between the children
+    // Case 3: [i, j]∩[A, B] ≠ ∅ but [i, j]⊄[A, B]
     int l = 2 * iNode;        // left child
     int r = 2 * iNode + 1;    // right child
     int m = (i + j) / 2;      // middle of interval
 
-    int bestLeft = minLeaf(l, i, m, A, B);
-    int bestRight = minLeaf(r, m + 1, j, A, B);
-
-    // return whichever is not -1 or the one whose stock value is the smallest
-    if (bestRight == -1 || bestLeft != -1 && stocksValues[bestLeft] <= stocksValues[bestRight])
-    {
-        return bestLeft;
-    }
-    else
-    {
-        return bestRight;
-    }
+    // return the best between the children
 }
 
 int main()
 {
-    std::cout << (stocksValues[60]);
 }
